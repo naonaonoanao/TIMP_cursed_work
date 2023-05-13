09b4d8a3c433d3c3df966b7d3703239d00e9c015
Ivankub <vanadolgun@gmail.com>
2023-05-13 17:11:27 +0700

    preview
    extr

--- scriptSearch.js
+++ scriptSearch.js
@@ -1,109 +1,119 @@
-roles=["manager", "ingeneer", "game", "senior", "inspector", "designer", "chief", "dancer", "director"]
+interests = new Set()
+names = new Set()
+roles = new Set()
+
+searching = new Set()
 
 last_response = null
 
 $(document).ready(function () {
-    prof_element  = $("#profile")
-    prof_width = parseFloat(prof_element.width())
-    prof_height = parseFloat(prof_element.height())
-    $("#nickname").css({
-        padding: 0.03*prof_width,
-    })
-    
-    phone_width = $("#phone").width()
-    phone_height = $("#phone").height()
+    getNames();
+    getRoles();
+    getInterests();
+    $("#make_search_btn").click(async function () {
+        requestIng = "http://127.0.0.1:5000/find_users_by_name?key=" + generateSearchRequest("names");
+        let request = await fetch(requestIng)
+        let responseNames = await request.json();
+        console.log(requestIng)
+        console.log(responseNames)
+        requestIng = "http://127.0.0.1:5000/find_users_by_roles?key=" + generateSearchRequest("roles");
+        request = await fetch(requestIng)
+        let responseRoles = await request.json();
+        console.log(requestIng)
+        console.log(responseRoles)
+    });
+});
 
-    $("#changePhoneButton").css({
-        padding: 0.03*phone_width,
-        height: phone_height-0.06*phone_width
-    })
-    $("#phoneNumber").css({
-        padding: 0.03*phone_width,
-        height: phone_height-0.06*phone_width
-    })
+function addToRoles(par) {
+    return roles.has(par)
+}
 
-    for (let i = 0; i < 30; i++) {
-        new_day = "<div class='day' id='day_"+(i+1)+"'>"+(i+1)+"</div>"
-        $("#calendar").append(new_day);
-        lastOne = $(".day")[$(".day").length-1]
-    }
-    $($(".day")[22]).addClass("deadline");
-    $(".day").click(function (e) { 
-        id = parseInt($(this).attr("id").split("_")[1])
-        //взять из бд
-        $("#task_day").text(id);
-        $("#task_name").text("имя таска на день: "+id)
-        $("#task_description").text("нереальное описание таска на день: "+id)
-        deadline = id+4
-        if (id <= 23){
-            deadline = 23
-        }
-        if (deadline > 30){
-            deadline = 30
-        }
-        $("#task_deadline").text(deadline)
-        $("#task_faster").text("")
-        text_last = "Это последний день выполнения!"
-        if ($(this).hasClass("deadline")){
-            for (let k = 0; k < text_last.length; k++) {
-                setTimeout(() => {
-                    $("#task_faster").text($("#task_faster").text()+text_last[k])
-                }, 15*k);
-                
+async function getNames() {
+    requestIng = "http://127.0.0.1:5000/find_users?key="
+    let request = await fetch(requestIng)
+    let response = await request.json();
+    for (let i = 0; i < response.count; i++) {
+        newNamePar = response.data[i].name.split(" ");
+        for (let n = 0; n < newNamePar.length; n++) {
+            if (names.has(newNamePar[n]) == false){
+                names.add(newNamePar[n]);
+                newCheck = "<div class='check'>" + newNamePar[n] + "</div>"
+                $("#names_check").append(newCheck);
             }
         }
-    });
-    $("#fast_search_button").click(async function (){
-        user_input = $("#fast_search").val()
-        requestIng = "http://127.0.0.1:5000/find_users?key=" + user_input
-        let request = await fetch(requestIng)
-        let response = await request.json();
-        console.log(response)
-        last_response = response
-        $("#dropdown_list").empty()
-        for (let i = 0; i < response.count; i++) {
-            new_element = "<div class='dropdown_field' id='profile_"+i+"'>" + response.data[i].name + "</div>"
-            $("#dropdown_list").append(new_element);
-        }
-    });
-    $("#cross_info").click(function () {
-        $("#person_info").css({
-            display: "none"
-        });
-    });
-});
+    }
+    console.log(names)
+}
 
+async function getRoles() {
+    requestIng = "http://127.0.0.1:5000/find_users?key="
+    let request = await fetch(requestIng)
+    let response = await request.json();
+    for (let i = 0; i < response.count; i++) {
+        newCheckPar = response.data[i].roles.split(" ")
+        for (let n = 0; n < newCheckPar.length; n++) {
+            if (roles.has(newCheckPar[n].toLowerCase()) == false){
+                roles.add(newCheckPar[n].toLowerCase());
+                newCheck = "<div class='check'>" + newCheckPar[n].toLowerCase() + "</div>"
+                $("#roles_check").append(newCheck);
+            }
+        }
+    }
+}
+
+async function getInterests() {
+    requestIng = "http://127.0.0.1:5000/find_users?key="
+    let request = await fetch(requestIng)
+    let response = await request.json();
+    for (let i = 0; i < response.count; i++) {
+        newCheckPar = response.data[i].interests.split(", ")
+        for (let n = 0; n < newCheckPar.length; n++) {
+            if (interests.has(newCheckPar[n].toLowerCase()) == false){
+                interests.add(newCheckPar[n].toLowerCase());
+                newCheck = "<div class='check'>" + newCheckPar[n].toLowerCase() + "</div>"
+                $("#interests_check").append(newCheck);
+            }
+        }
+    }
+}
 
 document.addEventListener("click", function (e) {
-    if ($(e.target).hasClass("dropdown_field")){
-        id = parseInt($(e.target).attr("id").split("_")[1])
-        person = last_response.data[id]
-        $("#prof_photo_info").css({
-            "background-image": "url("+person.photo+")"
-        });
-        console.log(person)
-        $("#naming_info").text(person.name)
-        $("#email_info").text(person.email)
-        $("#phoneNumber_info").text(person.number)
-        $("#roles_info").text(person.roles)
-        $("#departament_info").text(person.departament)
-        $("#interests_info").text(person.interests)
-        $("#person_info").css({
-            display: "block"
-        });
+    if ($(e.target).hasClass("search_tag")){
+        element = $(e.target).text() + "_" + $(e.target).attr("id")
+        $(e.target).remove();
+        searching.delete(element);
+    }
+    else if ($(e.target).hasClass("text_check")){
+        parent = $(e.target)[0].parentNode
+        if ($(parent).hasClass("opened") == false){
+            $(parent).addClass("opened");
+        }else{
+            $(parent).removeClass("opened");
+        }
+    }
+    else if ($(e.target).hasClass("check")){
+        myId = $($(e.target)[0].parentNode).attr("id").split("_")[0]
+        newtag = "<div class='search_tag' id='" + myId + "'>" + $(e.target).text() + "</div>"
+        search_parameter = $(e.target).text()+"_"+myId
+        if (searching.has(search_parameter) == false){
+            $("#searching_field").append(newtag);
+            searching.add(search_parameter)
+        }
     }
 });
 
-document.addEventListener("mousemove", function (e) {
-    if ($("#form_search").is(":hover") == false &&
-    $("#dropdown_list").is("hover") == false){
-        $("#dropdown_list").css({
-            "max-height": 0
-        })
+function generateSearchRequest(type = "") {
+    new_req = ""
+    console.log(searching)
+    for (let i = 0; i < searching.length; i++) {
+        console.log(searching[i])
+        if (searching[i].split("_")[1] == type){
+            if (i < searching.length-1){
+                new_req += (searching[i] + ",")
+            }else{
+                new_req += searching[i]
+            }
+        }
     }
-    else {
-        $("#dropdown_list").css({
-            "max-height": "40vh"
-        })
-    }
-});
+    return new_req
+}
