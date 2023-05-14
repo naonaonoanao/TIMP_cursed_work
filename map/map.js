774e9d031ef5ef53beab26586a07c92ef19500a1
Ivankub <vanadolgun@gmail.com>
2023-05-14 14:45:44 +0700

    beautified files
    ended!!!!!!

--- map/map.js
+++ map/map.js
@@ -1,6 +1,12 @@
-import { drawProductsMap } from "./productsMap.js";
-import {drawDepartamentMap} from './departmentMap.js'
-import {drawSubordinationMap} from './subordinationMap.js'
+import {
+    drawProductsMap
+} from "./productsMap.js";
+import {
+    drawDepartamentMap
+} from './departmentMap.js'
+import {
+    drawSubordinationMap
+} from './subordinationMap.js'
 
 
 
@@ -11,22 +17,22 @@ drawDepartamentMap();
 
 function createMap() {
     // Создаем карту Leaflet
- var map = L.map('map').setView([51.505, -0.09], 4);
+    var map = L.map('map').setView([51.505, -0.09], 4);
 
-map.setMinZoom(4);
-map.setMaxZoom(13);
+    map.setMinZoom(4);
+    map.setMaxZoom(13);
 
-// Создаем пустой слой
-var emptyLayer = L.tileLayer('', {
-    attribution: 'Empty Map'
-}).addTo(map);
-$(document.getElementsByClassName("leaflet-control-attribution leaflet-control")).remove();
-  return map
+    // Создаем пустой слой
+    var emptyLayer = L.tileLayer('', {
+        attribution: 'Empty Map'
+    }).addTo(map);
+    $(document.getElementsByClassName("leaflet-control-attribution leaflet-control")).remove();
+    return map
 }
 
-document.addEventListener("click", function (e) {
+document.addEventListener("click", function(e) {
     console.log(e);
-    if ($(e.target).hasClass("custom-cluster-icon")){
+    if ($(e.target).hasClass("custom-cluster-icon")) {
         console.log('Нажатие на иконку:', e.target);
     } else if ($(e.target).hasClass("teamCircle")) {
         console.log('Нажатие на иконку:', e.target);
@@ -35,19 +41,18 @@ document.addEventListener("click", function (e) {
         console.log('Нажатие на иконку:', e.target);
     }
     let el = e.target;
-    if ($(el).attr("id") == "toMyCab"){
+    if ($(el).attr("id") == "toMyCab") {
         console.log("ggg");
         window.location.href = "../index.html";
     }
-    if ($(el).hasClass("inText")){
+    if ($(el).hasClass("inText")) {
         console.log(changer_expand);
         if (changer_expand) {
             changer_expand = false;
             $("#map_changer").css({
                 "height": "7vh"
             })
-        }
-        else{
+        } else {
             changer_expand = true;
             $("#map_changer").css({
                 "height": "30vh"
@@ -58,18 +63,15 @@ document.addEventListener("click", function (e) {
             map.remove()
             map = createMap();
             drawDepartamentMap();
-        }
-        else if ($(el).attr("id") == "products") {
+        } else if ($(el).attr("id") == "products") {
             map.remove()
             map = createMap();
             drawProductsMap();
-        }
-        else if ($(el).attr("id") == "children") {
-            console.log('asajs')
+        } else if ($(el).attr("id") == "chldren") {
             map.remove()
             map = createMap();
             drawSubordinationMap();
         }
     }
-        
+
 });
